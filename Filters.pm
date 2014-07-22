3bdca3158c68d37358dbae8b3629831315560cac
Reini Urban <rurban@cpanel.net>
2014-07-22 08:42:48 -0500

    Release 0.15: best practices: our $VERSION, open $fh, no GLOB

--- Filters.pm
+++ Filters.pm
@@ -1,7 +1,7 @@
 package Inline::Filters;
 use strict;
 use Config;
-$Inline::Filters::VERSION = "0.14";
+our $VERSION = "0.15";
 use File::Spec;
 
 #============================================================================
@@ -120,12 +120,13 @@ sub Preprocess {
 
     my $tmpfile = File::Spec->catfile($ilsm->{API}{build_dir}, "Filters$$.c");
     $ilsm->mkpath($ilsm->{API}{build_dir});
-    open CSRC, ">", $tmpfile or die $!;
-    print CSRC $code;
-    close CSRC;
-    open PROCESSED, "$cpp $tmpfile |" or die $!;
-    $code = join '', <PROCESSED>;
-    close PROCESSED;
+    my ($CSRC, $PROCESSED);
+    open $CSRC, ">", $tmpfile or die $!;
+    print $CSRC $code;
+    close $CSRC;
+    open $PROCESSED, "$cpp $tmpfile |" or die $!;
+    $code = join '', <$PROCESSED>;
+    close $PROCESSED;
     unlink $tmpfile;
     return $code;
 }
